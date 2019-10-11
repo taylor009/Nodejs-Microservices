ffb7d4249f0de168ed24abff40cddb6f18d95156
Taylor Gagne <tgagne@netsurion.com>
2019-10-11 16:35:45 -0400

    refactored resolvers in th gateway.

--- packages/gateway/data/resolvers.js
+++ packages/gateway/data/resolvers.js
@@ -1,27 +1,20 @@
 'use strict';
-const axios = require('axios');
+const {serviceDatabase: {port}} = require('../config');
+const axios                     = require('axios');
 
-const getMails = async () =>
-{
-    return (await axios.get(`http://localhost:4001/mails`)).data.payload;
-};
+const hostName    = 'http://localhost';
+const databaseURL = `${hostName}:${port}`;
 
-const getSingleMail = async id =>
-{
-   return (await axios.get(`http://localhost:4001/mails/${id}`)).data.payload;
-};
+const get = async path => (await axios.get(`${databaseURL}/${path}`)).data.payload;
 
-const postSingleMail = async body =>
-{
-  return (await axios.post('http://localhost:4001/mails', {...body})).data.payload;
-};
+const post = async (path, body) => (await axios.post(`${databaseURL}/${path}`, {...body})).data.payload;
+
 
 module.exports = {
-    Query: {
-        mails: () => getMails(),
-        mail: (_, {id}) => getSingleMail(id)
-    },
-    Mutation: {
-        mail: (_, args) => postSingleMail(args)
+    Query      : {
+        mails: () => get('mails'),
+        mail: (_, {id}) => get(`mails/${id}`)
+    }, Mutation: {
+        mail: (_, args) => post('mails', args)
     }
 };
