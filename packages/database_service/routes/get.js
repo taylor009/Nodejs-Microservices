d0e661ce2892731114fb75b99ac7ba0890b3c7e6
Taylor Gagne <tgagne@netsurion.com>
2019-10-11 14:05:32 -0400

    Created fetch route to get the data from mongodb.

--- packages/database_service/routes/get.js
+++ packages/database_service/routes/get.js
@@ -1,19 +1,32 @@
 'use strict';
 const mongoose = require('mongoose');
-const Mail = mongoose.model('Mail');
+const Mail     = mongoose.model('Mail');
 
 module.exports = server =>
 {
-  server.get('/', async (_, res) =>
-  {
-     const m = new Mail({
-         subject: 'Hello Subj',
-         receiver: 'test@test.com',
-         content: 'Hello content'
-     });
+    server.get('/', async(_, res) =>
+    {
+        const m = new Mail({
+            subject: 'Hello Subj', receiver: 'test@test.com', content: 'Hello content'
+        });
 
-     await m.save();
+        try{
+            await m.save();
+        }catch(error)
+        {
+            console.log(error.message);
+        }
+        res.send('Worked');
+    })
 
-      res.send('Worked');
-  });
+    .get('/test', async (_, res) =>
+    {
+        let mails;
+       try{
+           mails = await Mail.find();
+       }catch(error){
+           console.log(error.message);
+       }
+        res.send(mails);
+    })
 };
