feae3e37faf734f5e0165785a38e836e261b0f46
Taylor Gagne <tgagne@netsurion.com>
2019-10-11 16:12:36 -0400

    created post function for the api.

--- packages/gateway/data/resolvers.js
+++ packages/gateway/data/resolvers.js
@@ -11,16 +11,17 @@ const getSingleMail = async id =>
    return (await axios.get(`http://localhost:4001/mails/${id}`)).data.payload;
 };
 
+const postSingleMail = async body =>
+{
+  return (await axios.post('http://localhost:4001/mails', {...body})).data.payload;
+};
+
 module.exports = {
     Query: {
         mails: () => getMails(),
         mail: (_, {id}) => getSingleMail(id)
     },
     Mutation: {
-        mail: (_, args) => {
-            mockMails[0] = args;
-
-            return args;
-        }
+        mail: (_, args) => postSingleMail(args)
     }
 };
