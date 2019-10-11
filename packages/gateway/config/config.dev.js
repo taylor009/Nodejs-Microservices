9e5f0eb91ddacddc15bdfa48760aa4a5adb0d67e
Taylor Gagne <tgagne@netsurion.com>
2019-10-11 15:35:58 -0400

    Connected database service to gateway service.

--- packages/gateway/config/config.dev.js
+++ packages/gateway/config/config.dev.js
@@ -1,6 +1,9 @@
 'use strict';
-const {PORT} = process.env;
+const {PORT, SERVICE_DB_PORT} = process.env;
 
 module.exports = {
-  port: PORT || 3000
+  port: PORT || 3000,
+  serviceDatabase: {
+    port: SERVICE_DB_PORT || 4000
+  }
 };
--- packages/gateway/data/resolvers.js
+++ packages/gateway/data/resolvers.js
@@ -1,25 +1,14 @@
 'use strict';
-const mockMails = [
-    {
-        subject: 'My first Email',
-        receiver: 'test@test.com',
-        content: 'hello world'
-    },
-    {
-        subject: 'My second Email',
-        receiver: 'test@test.com',
-        content: 'hello world'
-    },
-    {
-        subject: 'My third Email',
-        receiver: 'test@test.com',
-        content: 'hello world'
-    }
-];
+const axios = require('axios');
+
+const getMails = async () =>
+{
+    return (await axios.get(`http://localhost:4001/mails`)).data.payload;
+};
 
 module.exports = {
     Query: {
-        mails: () => mockMails,
+        mails: () => getMails(),
         mail: (_, args) => mockMails[0]
     },
     Mutation: {
